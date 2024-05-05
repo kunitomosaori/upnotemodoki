476b4b23f2c7f01402f4d288091fc9e2c22fa697
kunitomosaori <rinko.beagle@gmail.com>
2024-05-05 09:41:22 +0900

    "color change"

--- js/main.js
+++ js/main.js
@@ -24,3 +24,57 @@ $(document).ready(function () {
 
     updateList(); // 初期ロード時にリストを更新
 });
+
+// memoの背景変更
+$(document).ready(function() {
+    $('.bgBtn').click(function() {
+        $.cookie('bgColor', this.id, { expires: 7, path: '/' });
+        $('.bgBtn').removeClass('is_active');
+        switch (this.id) {
+            case 'bg-white':
+                bgWhite();
+                break;
+            case 'bg-blue':
+                bgBlue();
+                break;
+            case 'bg-black':
+                bgBlack();
+                break;
+        }
+    });
+
+    function bgWhite() {
+        $('#bg-white').addClass('is_active');
+        $('#memo').css('background', 'white');
+        $('#memo').css('color', 'black');
+    }
+
+    function bgBlue() {
+        $('#bg-blue').addClass('is_active');
+        $('#memo').css('background', 'blue');
+        $('#memo').css('color', 'white');
+    }
+
+    function bgBlack() {
+        $('#bg-black').addClass('is_active');
+        $('#memo').css('background', 'black');
+        $('#memo').css('color', 'white');
+    }
+
+    // 初回ロード時の処理
+    let bg = $.cookie('bgColor');
+    if (bg) {
+        $('.bgBtn').removeClass('is_active');
+        switch (bg) {
+            case 'bg-white':
+                bgWhite();
+                break;
+            case 'bg-blue':
+                bgBlue();
+                break;
+            case 'bg-black':
+                bgBlack();
+                break;
+        }
+    }
+});
